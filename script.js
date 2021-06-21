89b72cd92695fd6644a1007737589bf287ad1511
ntzwilly <ntzwilly@gmail.com>
2021-06-21 17:35:13 +0200

    Correct eslint errors

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const navDisplay = () => {
-  let burger = document.querySelector(".hamburger-container");
-  let nav = document.querySelector(".header-nav-link");
-  let main = document.querySelector(".main-background");
-  let logo = document.querySelector(".logo-image");
+  const burger = document.querySelector('.hamburger-container');
+  const nav = document.querySelector('.header-nav-link');
+  const main = document.querySelector('.main-background');
+  const logo = document.querySelector('.logo-image');
 
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-    burger.classList.toggle("toggle");
-    main.classList.toggle("foreground");
-    logo.classList.toggle("foreground");
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+    main.classList.toggle('foreground');
+    logo.classList.toggle('foreground');
   });
 };
 
