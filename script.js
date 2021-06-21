88169ebbcfff95373a725f626c742b4d16877933
ntzwilly <ntzwilly@gmail.com>
2021-06-21 15:29:53 +0200

    Add all grid container sections in main

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const navDisplay = () => {
-  let burger = document.querySelector('.hamburger-container');
-  let nav = document.querySelector('.header-nav-link');
+  let burger = document.querySelector(".hamburger-container");
+  let nav = document.querySelector(".header-nav-link");
+  let main = document.querySelector(".main-background");
 
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-    burger.classList.toggle('toggle');
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    burger.classList.toggle("toggle");
+    main.classList.toggle("foreground");
   });
-
-  
-}
+};
 
 navDisplay();
