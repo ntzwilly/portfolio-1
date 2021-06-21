a61995a71decaac4a1dd6fa9e166e94a44b89e0e
ntzwilly <ntzwilly@gmail.com>
2021-06-21 16:08:11 +0200

    Add blur to the background

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const navDisplay = () => {
-  let burger = document.querySelector(".hamburger-container");
-  let nav = document.querySelector(".header-nav-link");
-  let main = document.querySelector(".main-background");
+  let burger = document.querySelector('.hamburger-container');
+  let nav = document.querySelector('.header-nav-link');
+  let main = document.querySelector('.main-background');
+  let logo = document.querySelector('.logo-image');
 
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-    burger.classList.toggle("toggle");
-    main.classList.toggle("foreground");
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+    main.classList.toggle('foreground');
+    logo.classList.toggle('foreground');
   });
 };
 
