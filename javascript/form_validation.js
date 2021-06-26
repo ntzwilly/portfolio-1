45208e472121ef1e21c0e77e32b1a74f20c4c7c2
ntzwilly <ntzwilly@gmail.com>
2021-06-26 12:20:33 +0200

    Fix linter error

--- javascript/form_validation.js
+++ javascript/form_validation.js
@@ -1,7 +1,6 @@
 const contactForm = document.getElementById('form');
 const email = document.getElementById('email');
 const alertMessage = document.getElementById('span');
-const submitButton = document.getElementById('submit');
 
 contactForm.addEventListener('submit', (e) => {
   if (email.value.toLowerCase() !== email.value) {
