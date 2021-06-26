c025b39f0b8f1fe4821c26ecaae30a4a0644e744
ntzwilly <ntzwilly@gmail.com>
2021-06-26 10:29:29 +0200

    Fix linter errors

--- javascript/form_validation.js
+++ javascript/form_validation.js
@@ -1,6 +1,5 @@
 const contactForm = document.getElementById('form');
 const email = document.getElementById('email');
-console.log(email)
 const alertMessage = document.createElement('span');
 alertMessage.className = 'alert';
 alertMessage.textContent = 'Email can only be in lowercase';
@@ -8,7 +7,5 @@ contactForm.addEventListener('submit', (e) => {
   if (email.value.toLowerCase() !== email.value) {
     e.preventDefault();
     email.parentNode.insertBefore(alertMessage, email.nextSibling);
-  } else {
-    storeIt();
   }
 });
